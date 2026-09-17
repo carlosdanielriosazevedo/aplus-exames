bbe404ea9238bc16aea407f2b87d4d84fc439ec2
carlosdanielriosazevedo <carlosdanielriosazevedo@gmail.com>
2026-09-17 19:15:26 +0100

    Decompor grelhas de Português em observações

--- app/data/portugueseContent.js
+++ app/data/portugueseContent.js
@@ -4,10 +4,11 @@ import wave2 from "../../content/vnext/portuguese/foundation/portuguese-639-wave
 import wave3 from "../../content/vnext/portuguese/foundation/portuguese-639-wave3.json";
 import difficulty from "../../content/vnext/portuguese/foundation/portuguese-639-difficulty.json";
 import {applyPortugueseDifficulty} from "../lib/portugueseDifficulty.js";
+import {applyPortugueseRubricObservations} from "./portugueseRubrics.js";
 
 export const PORTUGUESE_CONTENT_PACKS=[pilot,wave1,wave2,wave3];
 export const PORTUGUESE_DIFFICULTY_MATRIX=difficulty;
-export const PORTUGUESE_ITEMS=applyPortugueseDifficulty(PORTUGUESE_CONTENT_PACKS.flatMap(pack=>pack.items),difficulty);
+export const PORTUGUESE_ITEMS=applyPortugueseRubricObservations(applyPortugueseDifficulty(PORTUGUESE_CONTENT_PACKS.flatMap(pack=>pack.items),difficulty));
 
 export function portugueseItemById(id){
   return PORTUGUESE_ITEMS.find(item=>item.id===id)||null;
