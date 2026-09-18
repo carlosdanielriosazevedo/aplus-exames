d537343446dc171b87ffc3404d3e26defef18cc1
carlosdanielriosazevedo <carlosdanielriosazevedo@gmail.com>
2026-09-18 11:24:47 +0100

    Expor rota do mini-exame de Português com texto partilhado

---
+++ app/portugues-mini-exame/page.js
@@ -0,0 +1,11 @@
+import PortuguesePassageMiniExam from "../components/PortuguesePassageMiniExam";
+import passageDocument from "../../content/vnext/portuguese/portuguese-639-passage-prototypes.json";
+import {buildPortuguesePassagePrototypeExam} from "../lib/portuguesePassages";
+import "./passage-mini-exam.css";
+
+export const metadata={title:"Mini-exame Português 639 · APProva+"};
+
+export default function PortuguesePassageMiniExamPage(){
+  const exam=buildPortuguesePassagePrototypeExam(passageDocument);
+  return <PortuguesePassageMiniExam exam={exam}/>;
+}
